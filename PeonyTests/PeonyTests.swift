2c88ae0aacf4e1072db44b3b041b00fcdc257933
James Kinsey <jameselijahk@icloud.com>
2025-10-22 19:36:23 -0700

    Initial Commit

---
+++ PeonyTests/PeonyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PeonyTests.swift
+//  PeonyTests
+//
+//  Created by James Kinsey on 10/22/25.
+//
+
+import Testing
+@testable import Peony
+
+struct PeonyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
